9f49d783a8797a02bf6a8dc33e5a4abc370e82e5
a <a>
2021-11-10 17:45:18 +0100

    arreglado sheet pageable

--- demo/src/main/java/com/example/demo/repository/SheetRepository.java
+++ demo/src/main/java/com/example/demo/repository/SheetRepository.java
@@ -3,13 +3,17 @@ package com.example.demo.repository;
 import com.example.demo.entity.Sheet;
 import com.example.demo.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface SheetRepository extends JpaRepository<Sheet,Long> { //la task y la id
+public interface SheetRepository extends PagingAndSortingRepository<Sheet,Long> { //la task y la id
 
 
     Optional<Sheet> findById(Long id);
+
+    List<Sheet> findAll();
 }
