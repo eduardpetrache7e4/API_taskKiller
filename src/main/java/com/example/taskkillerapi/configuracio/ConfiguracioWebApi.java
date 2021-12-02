196bae843d6705cc1c742d75993557c10e198736
eduard <eduard.petrache.7e4@itb.cat>
2021-12-02 17:05:58 +0100

    asdf

---
+++ src/main/java/com/example/taskkillerapi/configuracio/ConfiguracioWebApi.java
@@ -0,0 +1,25 @@
+package com.example.taskkillerapi.configuracio;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ConfiguracioWebApi {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        //.allowedOrigins("http://localhost:9001")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .maxAge(3600);
+            }
+
+        };
+    }
+}
